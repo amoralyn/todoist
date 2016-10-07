a34c56a008554eebeca8fd1741028a171abd03a9
andela-lovelyntijesunimi-israel <lovelyn.tijesunimi-israel@andela.com>
2016-10-07 19:41:53 +0100

    Add subTask model

--- server/model/subtask.model.js
+++ server/model/subtask.model.js
@@ -2,6 +2,7 @@
   'use strict';
   const mongoose = require('mongoose');
   const Schema = mongoose.Schema;
+  const ObjectId = Schema.Types.ObjectId;
 
 
   let subTaskSchema = new Schema({
@@ -29,6 +30,6 @@
   });
 
 
-  let SubTask = mongoose.model('SubTask', taskSchema);
+  let SubTask = mongoose.model('SubTask', subTaskSchema);
   module.exports = SubTask;
 })();
